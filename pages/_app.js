e1d3cc8b9b4a35d5dca5e13ab30325612e883d23
Filipe Oliveira <contato@fmoliveira.com.br>
2020-08-23 01:33:57 -0300

    Add fork ribbon with link to repository.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <a href="https://github.com/fmoliveira/poc-doc-with-algolia" style={{ position: 'absolute', top: 0, right: 0 }}>
+        <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" />
+      </a>
+    </>
+  );
 }
 
 export default MyApp
